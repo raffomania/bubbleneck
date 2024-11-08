6cc7e7aefde0c7c396474770a76b268800b16901
Rafael Epplée <hello@rafa.ee>
2024-11-08 20:57:50 +0100

    re-enable keyboard controls

--- player.gd
+++ player.gd
@@ -12,7 +12,11 @@ func _draw() -> void:
     draw_circle(position, 20, Color.WEB_GREEN)
 
 func _process(delta: float) -> void:
-    var dir = Vector2(1, 0) * Input.get_joy_axis(device, JOY_AXIS_LEFT_X)
-    dir.y = Input.get_joy_axis(device, JOY_AXIS_LEFT_Y)
+    var dir
+    if device == -1:
+        dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+    else:
+        dir = Vector2(1, 0) * Input.get_joy_axis(device, JOY_AXIS_LEFT_X)
+        dir.y = Input.get_joy_axis(device, JOY_AXIS_LEFT_Y)
 
     position += dir * delta * movespeed
