0f93ae5cc69c7ae5d35a9b143232ae7927b626b0
Rafael Epplée <hello@rafa.ee>
2024-11-08 20:06:31 +0100

    Add multiplayer input

--- player.gd
+++ player.gd
@@ -1,12 +1,16 @@
 extends Node2D
 
+@export
+var device: int = -1
+@export
+var movespeed := 700
 
 func _draw() -> void:
     draw_circle(position, 20, Color.WEB_GREEN)
 
 
 func _process(delta: float) -> void:
-    var dir = Vector2(1, 0) * Input.get_axis("ui_left", "ui_right")
-    dir.y += Input.get_axis("ui_up", "ui_down")
+    var dir = Vector2(1, 0) * MultiplayerInput.get_axis(device, "ui_left", "ui_right")
+    dir.y += MultiplayerInput.get_axis(device, "ui_up", "ui_down")
 
-    position += dir * delta * 700
+    position += dir * delta * movespeed
