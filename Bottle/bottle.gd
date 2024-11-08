a53f19fa7722a50f7bce29b9d66d780f7672e258
Merle Hoffmann <hoffmann@subshell.com>
2024-11-08 21:14:35 +0100

    add: bottle rotation speed

--- Bottle/bottle.gd
+++ Bottle/bottle.gd
@@ -1,15 +1,36 @@
 extends Node2D
 
+# Describes whether the Bottle is rotating at all.
+var rotating = true
+# The current rotational speed of the bottle
+var rotation_speed: float = 0.5
+# The max speed with which the bottle can rotate.
+# `1` equals rotation per second in radians.
+var max_rotation_speed: float = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-  # center the bottle
-  var viewport = get_viewport_rect()
+    # center the bottle
+    var viewport = get_viewport_rect()
 
-  position.x = viewport.size.x / 2
-  position.y = viewport.size.y / 2
-
-func _process(delta) -> void:
-  rotation += 0.5 * delta
+    position.x = viewport.size.x / 2
+    position.y = viewport.size.y / 2
 
 
+func _process(delta: float) -> void:
+    if rotating:
+        rotation += rotation_speed * delta
+
+
+# Adds an impulse to the rotation of the bottle.
+# The impulse cannot be faster than `max_rotation_speed`.
+#
+# `impulse`: equals the added rotations per second in radian.
+func add_impulse(impulse: float) -> void:
+    rotation_speed += impulse
+
+    # Limit the bottle rotation to the max possible speed
+    if rotation_speed > max_rotation_speed:
+        rotation_speed = max_rotation_speed
+    elif rotation_speed < max_rotation_speed:
+        rotation_speed = -max_rotation_speed
