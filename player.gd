7b6f4012647f394144c9b86607931b0324b2dfa1
Rafael Epplée <hello@rafa.ee>
2024-11-08 20:53:27 +0100

    Remove local multiplayer plugin

--- player.gd
+++ player.gd
@@ -1,16 +1,18 @@
 extends Node2D
 
 @export
-var device: int = 1
+var device := 0
 @export
 var movespeed := 700
+
+func _ready():
+    print(Input.get_connected_joypads())
 
 func _draw() -> void:
     draw_circle(position, 20, Color.WEB_GREEN)
 
-
 func _process(delta: float) -> void:
-    var dir = Vector2(1, 0) * MultiplayerInput.get_axis(device, "ui_left", "ui_right")
-    dir.y += MultiplayerInput.get_axis(device, "ui_up", "ui_down")
+    var dir = Vector2(1, 0) * Input.get_joy_axis(device, JOY_AXIS_LEFT_X)
+    dir.y = Input.get_joy_axis(device, JOY_AXIS_LEFT_Y)
 
     position += dir * delta * movespeed
