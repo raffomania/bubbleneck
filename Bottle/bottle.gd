8dd4634b16d98633bb88eb559910cf1580ae9eee
Merle Hoffmann <hoffmann@subshell.com>
2024-11-08 22:12:48 +0100

    add: pop countdown

--- Bottle/bottle.gd
+++ Bottle/bottle.gd
@@ -1,7 +1,13 @@
 extends Node2D
 
-# Describes whether the Bottle is rotating at all.
-var rotating = true
+# Describes whether the Bottle is popped.
+var popped = false
+# Seconds until bottle pops.
+var pop_countdown = 0
+# Min/Max Time until bottle pops.
+var pop_countdown_min = 30
+var pop_countdown_max = 60
+
 # The current rotational speed of the bottle
 var rotation_speed: float = 0.5
 # The max speed with which the bottle can rotate.
@@ -16,11 +22,33 @@ func _ready() -> void:
     position.x = viewport.size.x / 2
     position.y = viewport.size.y / 2
 
+    var rng = RandomNumberGenerator.new()
+    rng.seed = Time.get_unix_time_from_system()
+    pop_countdown = rng.randi_range(pop_countdown_min, pop_countdown_max)
+
 
 func _process(delta: float) -> void:
-    if rotating:
+    if not popped:
         rotation += rotation_speed * delta
+        pop_countdown -= delta
 
+    # Check if the bottle should pop.
+    if pop_countdown <= 0:
+        popped = true
+
+
+# Call this to hit the bottle.
+# Reduces the countdown until pop and adds some impulse to the bottle.
+#
+# `impulse`: equals the added rotations per second in radian.
+# `countdown_reduction` (Optional): Set an explicit value for the countdown reduction.
+#                                   If not provided, the impulse will be used to calculate this value.
+func hit(impulse: float, countdown_reduction = null) -> void:
+    if countdown_reduction != null:
+        pop_countdown -= countdown_reduction
+    else:
+        pop_countdown -= impulse
+    add_impulse(impulse)
 
 # Adds an impulse to the rotation of the bottle.
 # The impulse cannot be faster than `max_rotation_speed`.
@@ -34,3 +62,4 @@ func add_impulse(impulse: float) -> void:
         rotation_speed = max_rotation_speed
     elif rotation_speed < max_rotation_speed:
         rotation_speed = -max_rotation_speed
+
