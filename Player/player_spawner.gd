b4a681538ffd646f3f060ab0d561c7ea5e23f114
Karim Djemai <karimdjemai1997@gmail.com>
2024-11-09 14:02:45 +0100

    fix color generation when controller is connected during game

--- Player/player_spawner.gd
+++ Player/player_spawner.gd
@@ -12,36 +12,38 @@ var player_scene = preload("res://Player/player.tscn")
 func _ready() -> void:
     # Wait until everything's ready
     await get_tree().create_timer(0.1).timeout
-    #+ 2 for keyboard
-    var num_devices = Input.get_connected_joypads().size() + 2
 
     for device in Input.get_connected_joypads():
-        spawn_player(device, num_devices)
+        spawn_player(device)
 
     # Spawn keyboard players
     for device in [-1, -2]:
-        spawn_player(device, num_devices)
+        spawn_player(device)
 
-    Input.joy_connection_changed.connect(self.joy_connection_changed, num_devices)
+    Input.joy_connection_changed.connect(self.joy_connection_changed)
 
-func joy_connection_changed(device, connected: bool, num_devices: int):
+func joy_connection_changed(device, connected: bool):
     if connected and device not in spawned_devices:
-        spawn_player(device, num_devices)
+        spawn_player(device)
 
-func spawn_player(device: int, num_devices: int):
-    print("spawning player with device ", device, " and num_devices ", num_devices)
+func spawn_player(device: int):
+    print("spawning player with device ", device)
        
     var player = player_scene.instantiate()
     player.name = "Player" + str(device)
     player.device = device
-    player.player_color = random_player_color(device, num_devices)
+    player.player_color = random_player_color(device)
     player.global_position = get_viewport_rect().size / 2
     $'..'.add_child(player)
     spawned_devices.append(device)
 
-func random_player_color(player_index: int, num_players: int):
+func random_player_color(player_index: int):
     # +2 because of 2 negative keyboard devices
-    var hue = (float(player_index + 2) / float(num_players))
-    # print("hue: ", hue, " player_index: ", player_index, " num_players: ", num_players)
+    var color_index = (player_index + 2) % 5
+
+    var hue = color_index / 5.0
+    if (player_index + 2) > 5:
+        hue += 1/10.0
+
     return Color.from_hsv(hue, 0.8, 0.9, 1)
 
