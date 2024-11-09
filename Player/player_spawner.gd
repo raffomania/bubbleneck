c82bed994f5014a2b9cc3a0685225b4cc7f8044f
Karim Djemai <karimdjemai1997@gmail.com>
2024-11-09 01:46:36 +0100

    nevermind, revert the fix and descibe the problem better

--- Player/player_spawner.gd
+++ Player/player_spawner.gd
@@ -3,14 +3,16 @@ extends Node2D
 var spawned_devices = []
 var player_scene = preload("res://Player/player.tscn")
 
-# TODO check this again tomorrow
-# this fixes a problem on my device where 2 players are spawned
-# outside of the arena bounds and after they die, they respawn outside again
-# the fix seems to work because _Input() is called before _ready()
-func _Input() -> void:
+# this incurs weird behavior for me that the others don't have
+# the players spawrned from this are spawned outside the arena bounds
+# and if they die, they respawn outside again, 
+# their whole transform dimensions seems to be shifted
+# it is prob. related to time time when this function is called (too early for the viewport rect?)
+# also it is different on my end because my mouse/trackpad seems to be recognized as an input device
+func _ready() -> void:
     for device in Input.get_connected_joypads():
         spawn_player(device)
-    
+
     Input.joy_connection_changed.connect(self.joy_connection_changed)
 
 func joy_connection_changed(device, connected: bool):
@@ -20,6 +22,9 @@ func joy_connection_changed(device, connected: bool):
 func spawn_player(device: int):
     print("spawning player with device ", device)
     var player = player_scene.instantiate()
+    player.name = "Player" + str(device)
     player.device = device
+    player.position = get_viewport_rect().size / 2
+    player.player_color = Color.from_hsv(randf(), randf(), randf(),1)
     add_child(player)
     spawned_devices.append(device)
