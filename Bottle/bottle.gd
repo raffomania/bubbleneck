c5017c1de21afccd8ca058579f94849fea07c3dc
Merle Hoffmann <hoffmann@subshell.com>
2024-11-10 00:13:50 +0100

    change countdown speed according to rotation speed

--- Bottle/bottle.gd
+++ Bottle/bottle.gd
@@ -6,9 +6,13 @@ class_name Bottle
 var popped = false
 # Seconds until bottle pops.
 var pop_countdown = 0
+var pop_countdown_start = 0
 # Min/Max Time until bottle pops.
-var pop_countdown_min = 10
-var pop_countdown_max = 20
+var pop_countdown_min = 30
+var pop_countdown_max = 60
+# Min/Max rate at which the countdown runs.
+var pop_countdown_min_speed = 0.5
+var pop_countdown_max_speed = 1.5
 
 # The current rotational speed of the bottle
 var rotation_speed: float = 0.5
@@ -18,6 +22,8 @@ var max_rotation_speed: float = 1
 
 var bottleneck_particles: GPUParticles2D
 var pop_particles: GPUParticles2D
+var max_inner_particle_lifetime = 10
+var min_inner_particle_lifetime = 0.1
 var inside_particles: GPUParticles2D
 var player_has_entered := false
 
@@ -47,6 +53,7 @@ func _ready() -> void:
     var rng = RandomNumberGenerator.new()
     rng.seed = Time.get_unix_time_from_system()
     pop_countdown = rng.randi_range(pop_countdown_min, pop_countdown_max)
+    pop_countdown_start = pop_countdown
 
     bottleneck_particles = $BottleneckParticles
     pop_particles = $PopParticles
@@ -62,7 +69,15 @@ func _ready() -> void:
 func _process(delta: float) -> void:
     if not popped:
         rotation += rotation_speed * delta
-        pop_countdown -= delta
+
+        # Calculate countdown and countdown speed depending on rotation speed.
+        var pop_countdown_speed = (abs(rotation_speed) / max_rotation_speed) * pop_countdown_max_speed + pop_countdown_min_speed
+        pop_countdown -= delta * pop_countdown_speed
+        #print("Countdown: %s, Speed: %s, Rotation: %s" % [pop_countdown, pop_countdown_speed, rotation_speed])
+
+        # Calculate particle lifetime depending on pop countdown.
+        var countdown_percentage = pop_countdown / pop_countdown_start
+        inside_particles.lifetime = max_inner_particle_lifetime * countdown_percentage + min_inner_particle_lifetime
 
         if rotation_speed > 0:
             rotation_speed -= delta * 0.05
@@ -94,13 +109,7 @@ func pop_bottle() -> void:
 # Reduces the countdown until pop and adds some impulse to the bottle.
 #
 # `impulse`: equals the added rotations per second in radian.
-# `countdown_reduction` (Optional): Set an explicit value for the countdown reduction.
-#                                   If not provided, the impulse will be used to calculate this value.
-func hit(impulse: float, countdown_reduction = null) -> void:
-    if countdown_reduction != null:
-        pop_countdown -= countdown_reduction
-    else:
-        pop_countdown -= impulse
+func hit(impulse: float) -> void:
     add_impulse(impulse)
 
 # Adds an impulse to the rotation of the bottle.
