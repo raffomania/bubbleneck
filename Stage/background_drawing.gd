ab68c496811975ef5997b749f7fbdeeda3b7aca0
Rafael Epplée <hello@rafa.ee>
2024-11-10 16:24:13 +0100

    fix splotches

--- Stage/background_drawing.gd
+++ Stage/background_drawing.gd
@@ -1,22 +1,14 @@
 extends Node2D
 
-var kill_queue = []
+var splotch_scene = preload("res://Stage/splotches.tscn")
 
 func _ready() -> void:
     Globals.player_killed.connect(player_killed)
 
 func player_killed(player: Player):
-    kill_queue.append(player)
-    queue_redraw()
-
-func _draw():
-    for player in kill_queue:
-        var color: Color = player.player_color.darkened(0.3)
-        color.a = 0.5
-        for i in range(0, 20):
-            var offset = Vector2(randf_range(-1, 1) * 50, randf_range(-1, 1) * 50)
-            var radius = randf_range(2, 26)
-
-            draw_circle(player.global_position + offset, radius, color, true, -1, true)
-
-    kill_queue = []
+    var color: Color = Color(player.player_color).darkened(0.3)
+    color.a = 0.5
+    var splotches = splotch_scene.instantiate()
+    splotches.color = color
+    splotches.global_position = Vector2(player.global_position)
+    add_child(splotches)
---
+++ Stage/splotches.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+@export
+var color: Color
+
+func _ready() -> void:
+    queue_redraw()
+
+func _draw():
+    print("draw")
+    for i in range(0, 20):
+        var offset = Vector2(randf_range(-1, 1) * 50, randf_range(-1, 1) * 50)
+        var radius = randf_range(2, 26)
+
+        draw_circle(offset, radius, color, true, -1, true)
