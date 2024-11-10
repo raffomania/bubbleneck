36a589f8157509b820526fded10df8a422ed63a7
Rafael Epplée <hello@rafa.ee>
2024-11-10 16:43:13 +0100

    add a safety guard

--- Stage/background_drawing.gd
+++ Stage/background_drawing.gd
@@ -6,6 +6,9 @@ func _ready() -> void:
     Globals.player_killed.connect(player_killed)
 
 func player_killed(player: Player):
+    if not is_instance_valid(player):
+        print("WARNING: invalid player passed to global player killed signal")
+        return
     var color: Color = Color(player.player_color).darkened(0.3)
     color.a = 0.5
     var splotches = splotch_scene.instantiate()
