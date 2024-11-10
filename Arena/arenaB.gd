9daefe2b2555bcc62d54b0f88179ab2755888c00
Patrice Epplee <patrice.epplee@gmail.com>
2024-11-10 10:50:16 +0100

    rect arena boundaries

--- Arena/arenaB.gd
+++ Arena/arenaB.gd
@@ -1,10 +1,14 @@
 extends Node2D
 
+@onready
+var arena_offset := 5.0
 
 var radius = 0
 var center = Vector2(0, 0)
 var viewport : Rect2
 var arena_size : Vector2
+
+
 
 func _ready() -> void:
     viewport = get_viewport_rect()
@@ -15,11 +19,11 @@ func _ready() -> void:
     arena_size = Vector2(Vector2(viewport.size.x - 10.0, viewport.size.y - 10.0))
 
 func _draw() -> void:
-    draw_rect(Rect2(Vector2(-viewport.size.x / 2.0 + 5.0, -viewport.size.y / 2.0 + 5.0), arena_size), Color(58 / 256.0, 81 / 256.0, 120 / 256.0))
+    draw_rect(Rect2(Vector2(-viewport.size.x / 2.0 + arena_offset, -viewport.size.y / 2.0 + arena_offset), arena_size), Color(58 / 256.0, 81 / 256.0, 120 / 256.0))
 
 
 func is_point_outside_rect(point) -> bool:
-    if (point.x >= arena_size.x or point.x <= arena_size.x or point.y >= arena_size.y or point.y <= arena_size.y):
+    if (point.x >= arena_size.x + arena_offset or point.x <= arena_offset or point.y >= arena_size.y + arena_offset or point.y <= arena_offset):
         return true
     else:
         return false
@@ -30,12 +34,10 @@ func _process(_delta: float) -> void:
     center.x = viewport.size.x / 2
     center.y = viewport.size.y / 2
     for player: Node2D in get_tree().get_nodes_in_group('players'):
-       var dist = player.global_position.distance_to(center)
-       if (!player.dead and dist > radius - player.radius):
-           player.position -= 0.01 * (player.position - center)
+       if (!player.dead and is_point_outside_rect(player.global_position)):
+           player.global_position -= 0.01 * (player.global_position - center)
 
     for node: Node2D in get_tree().get_nodes_in_group('weapons'):
        var weapon = node as Weapon
-       var dist = weapon.global_position.distance_to(center)
-       if (weapon.is_throwing and dist > radius - 10):
+       if (weapon.is_throwing and is_point_outside_rect(weapon.global_position)):
            weapon.stick()
