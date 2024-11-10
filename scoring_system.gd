2c53207432280d38bc509d9aa243f3d86b15fe7f
C-gotoh <shigotoh1@gmail.com>
2024-11-10 16:49:07 +0100

    Increase player device indices to prevent negative numbers

--- scoring_system.gd
+++ scoring_system.gd
@@ -8,12 +8,12 @@ var score_text = preload("res://Scoring/score_text.tscn")
 var childs = {}
 
 var colors = {
-    -2: 'Cyan',
-    -1: 'Orange',
-    0: 'Purple',
-    1: 'Pink',
-    2: 'Green',
-    3: 'Cyan',
+    0: 'Cyan',
+    1: 'Orange',
+    2: 'Purple',
+    3: 'Pink',
+    4: 'Green',
+    5: 'Cyan',
 }
 
 
@@ -23,24 +23,27 @@ func _ready() -> void:
 func init_scores() -> void:
     for key in childs.keys():
         remove_child(childs[key])
+
     for node in get_tree().get_nodes_in_group('players'):
         var player = node as Player
-        if !scores.keys().has(player.device):
-            scores[player.device] = 0
+        var index = player.device + 2
+        if !scores.keys().has(index):
+            scores[index] = 0
         var score = score_text.instantiate()
         var text = score as RichTextLabel
-        var colorname = colors[player.device]
-        text.text = 'Player %s:   %s \n' % [colorname, scores[player.device]]
-        text.global_position = Vector2(0, 50 * (player.device + 3))
+        var colorname = colors[index]
+        text.text = 'Player %s:   %s \n' % [colorname, scores[index]]
+        text.global_position = Vector2(0, 50 * (index + 3))
         text.modulate = player.player_color
-        childs[player.device] = score
+        childs[index] = score
         add_child(score)
         queue_redraw()
 
 
 func increase_score(player):
-    if scores.keys().has(player.device):
-        scores[player.device] += 1
+    var index = player.device + 2
+    if scores.keys().has(index):
+        scores[index] += 1
     else:
-        scores[player.device] = 1
+        scores[index] = 1
     init_scores()
